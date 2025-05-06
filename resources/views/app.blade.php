28a3ae7f883a819961c54fc45110e9046a6f6572
Veloxium <veloxium.09@gmail.com>
2025-05-07 01:46:19 +0800

    Update dependencies and create app view template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="min-h-screen flex justify-center items-center">
+        <a href="#" class="text-4xl font-semibold cursor-pointer hover:underline text-blue-500">
+            Hello world!
+        </a>
+    </div>
+</body>
+
+</html>
