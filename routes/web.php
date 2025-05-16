e2c32e414152a990b45e09cf6ecbf24e38b0c0fa
Gylmynnn <muhamadgilman999@gmail.com>
2025-05-16 23:13:34 +0800

    core : include all route directory

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+require __DIR__ . '/modules/homepage.php';
+require __DIR__ . '/modules/portofolio.php';
+require __DIR__ . '/modules/category.php';
+require __DIR__ . '/modules/reward.php';
+require __DIR__ . '/modules/authentication.php';
+require __DIR__ . '/modules/approval.php';
+require __DIR__ . '/modules/dashboard.php';
+require __DIR__ . '/modules/report.php';
+require __DIR__ . '/modules/statistic.php';
 
-Route::get('/', function () {
-    return view('app');
-});
-
-Route::get('/category', function () {
-    return view('guest.category');
-});
-
-Route::get('/portofolio', function () {
-    return view('guest.portofolio');
-});
-
-Route::get('/category/detail', function () {
-    return view('guest.detail_category');
-});
